282778dcf34c66698dbbdd7370a934f911bb2d0a
ntnquang2906 <quangntn2906@gmail.com>
2026-06-05 16:54:20 +0700

    Update user credentials - Cập nhật tên tài khoản

--- credentials.php
+++ credentials.php
@@ -2,8 +2,8 @@
 // Danh sách tài khoản cho lãnh đạo/quản trị viên
 $accounts = [
     'admin' => 'dhqg2026',
-    'Tu' => 'Tuvnu2026',
-    'Son' => 'Sonvnu2026'
+    'Tuttt' => 'Tuvnu2026',
+    'Sonpb' => 'Sonvnu2026'
 ];
 
 // Nếu cần thêm tài khoản mới, thêm vào array trên theo format: 'username' => 'password'
